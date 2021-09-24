4270c1ddaa03966099a948af2534dfe468bfe799
Jodick Reyes <jodickreyes28@gmail.com>
2021-09-24 21:22:02 +0800

    Updating pending number

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,12 @@
 const container = document.querySelector('#main-container'); // select the main div or parent element div in html
 const addButton = document.querySelector('.button-add'); // select the button element from the html by class name
 const todoInput = document.querySelector('.todo-input'); // select the input element from the html by class name
-const buttonDelete = document.querySelector('.button-delete')
 const pendingNumber = document.querySelector('.pendingNumber');
 
 // adding event listener to the addButton to get the value of input and to return in the addTodo Function
 addButton.addEventListener('click', () => {
     addTodo(todoInput.value);
+    renderFunction();
     return todoInput.onkeyup();
 });
 
@@ -32,8 +32,6 @@ const addTodo = (todo) => { // create a function to perform a task by adding val
     span.classList.add('input-item'); // set the id of span by Date.now(), Date.now() is a method that returns the number of second from the date
     span.textContent = todo; // getting the value of todoInput using textContent and add in html
 
-
-
     const deleteButton = document.createElement('button');
     deleteButton.classList.add('delete-button');
     deleteButton.textContent = "Delete";
@@ -47,8 +45,11 @@ const addTodo = (todo) => { // create a function to perform a task by adding val
     
     deleteButton.addEventListener('click', () => {
         container.removeChild(div);
+        renderFunction();
     });
 
-    pendingNumber.innerHTML = container.childElementCount;
+}
 
+const renderFunction = () => {
+    pendingNumber.innerHTML = container.childElementCount;
 }
